796ea25dcbb3c4ac1a083a93601c13bce8ace7cd
angelinaturchyn <angelinaturchyn@gmail.com>
2022-02-15 22:36:35 -0600

    add articles add tags add likes count

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -13,7 +13,7 @@ describe('Test with backend', () => {
         cy.route('POST', '**/articles').as('postArticles')
 
         cy.contains(' New Article ').click()
-        cy.get('[placeholder="Article Title"]').type('COOL KAts')
+        cy.get('[placeholder="Article Title"]').type('your KAtsss')
         cy.get('[placeholder="What\'s this article about?"]').type('NOTHIIIIIINGGGGGGSS')
         cy.get('[placeholder="Write your article (in markdown)"]').type('wowowowwww!!!!!!!!!!!!!!!!!!')
         cy.contains(' Publish Article ').click()
@@ -27,10 +27,32 @@ describe('Test with backend', () => {
         })
     })
 
-    it.only('Should show new tags',() =>{
+    it('Should show new tags',() =>{
         cy.get('[class="sidebar"]')
             .should('contain','Code')
             .and('contain','Like')
             .and('contain','SpeedyGonzales')
     })
+
+    it.only('verify global feed likes count', () => {
+
+        cy.route('GET', '**/articles/feed', '{"articles":[],"articlesCount":0}')
+        cy.route('GET', '**/articles', 'fixtures:articles.json')
+
+        cy.contains('Global Feed').click()
+        cy.get('app-article-list button').then( listOfButtons => {
+            expect(listOfButtons[0]).to.contain('0')
+            expect(listOfButtons[1]).to.contain('0')
+        })
+
+        cy.fixture('articles').then(file => {
+         const articleLink = file.articles[0].slug
+            cy.route('POST', '**/articles/'+articleLink+'/favorite', file)
+        })
+
+        cy.get('app-article-list button').eq(0).click().should('contain', 1)
+
+    })
+
+
 })
